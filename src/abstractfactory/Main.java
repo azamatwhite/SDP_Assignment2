c43c3b0303371329b7c5c34f6e3492d406d66749
Azamat Zhiyembetov <ashiembetov@gmail.com>
2026-09-20 23:15:13 +0500

    Final change in client

--- src/abstractfactory/Main.java
+++ src/abstractfactory/Main.java
@@ -1,7 +1,15 @@
 package abstractfactory;
 
+import abstractfactory.homevillage.HomeVillageFactory;
+
 public class Main {
     public static void main(String[] args){
+
+        LocationFactory factory = new HomeVillageFactory();
+
+        Location location = new Location(factory);
+        location.visit();
+
 
     }
 }
